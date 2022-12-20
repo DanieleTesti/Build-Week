5b371137dad742b029117ec84780c955c5bd56d7
DanieleTesti <danieletesti14@gmail.com>
2022-12-20 11:51:07 +0100

    pag4 update

--- pag4.js
+++ pag4.js
@@ -1,8 +1,20 @@
 function stelle() {
   for (let i = 0; i <= 10; i++) {
-    const star = document.getElementById("main div+div");
+    const star = document.getElementById("main #star");
+    // foto = document.createElement(" ");
+    // (foto.src = " width="),
+    //   47,
+    //   " height=",
+    //   46,
+    //   " viewBox=",
+    //   0,
+    //   0,
+    //   47,
+    //   46,
+    //   " fill=",
+    //   none;
     foto = document.createElement("img");
-    foto.src = "img /assets/star.svg";
+    foto.src = "./assets/star.svg";
     star.appendChild(foto);
   }
 }
